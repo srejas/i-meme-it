c48657629de615dbd6b41b3038222358a902c893
srejas <srejas@outlook.com>
2026-06-08 17:39:31 -0600

    Comments added to code.

--- src/js/DataServices.mjs
+++ src/js/DataServices.mjs
@@ -1,7 +1,10 @@
+// The two APIs that will be used to fetch trending memes and search for memes based on user input
 const trendingURL = 'https://api.imgflip.com/get_memes';
 const searchURL = 'https://justmeme.wtf/api/v1/templates/search?q=';
 
+// DataServices class that handles any and all interactions with the APIs.
 export default class DataServices {
+    // Method to fetch trending memes from the API. Returns a promise that resolves to an array of meme objects.
     async getTrendingMemes() {
         try {
             const response = await fetch(trendingURL);
@@ -16,6 +19,7 @@ export default class DataServices {
         }
     }
 
+    // Method to search for memes based on user input. Returns a promise that resolves to an array of meme objects.
     async getSearchedMemes(searchTerm) {
         try {
             const response = await fetch(searchURL + encodeURIComponent(searchTerm));
