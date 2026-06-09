6e251eb763c4690f52340aeb57ddb4affa40d8e0
srejas <srejas@outlook.com>
2026-06-09 12:57:20 -0600

    Comments added for clarity

--- src/js/DisplayServices.mjs
+++ src/js/DisplayServices.mjs
@@ -1,3 +1,4 @@
+// A module for rendering lists of items to a specified DOM container using a template function. It accepts a list of items, a template function to generate HTML for each item, and a container element where the generated HTML will be inserted. It also allows for specifying the position of the inserted HTML and whether to clear the container before inserting new content.
 export function renderListWithTemplate(list, templateFn, container, position = 'afterbegin', clear = false) {
     if (clear) {
         container.innerHTML = '';
@@ -6,10 +7,11 @@ export function renderListWithTemplate(list, templateFn, container, position = '
     container.insertAdjacentHTML(position, htmlStrings.join(''));
 }
 
+// A template function specifically for rendering meme results. It takes a meme object and returns an HTML string representing a list item with the meme's image and name.
 export function resultsTemplate(result) {
     return `
     <li class="meme-card">
-        <a href="./edit.html">
+        <a href="./edit">
             <img src="${result.url}" alt="${result.name}">
         </a>
     </li>`;
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,12 @@
 import DataServices from './DataServices.mjs';
 import { renderListWithTemplate, resultsTemplate } from './DisplayServices.mjs';
 
+// Initialize a new instance of the DataServices class to make sure we're working with the most up-to-date data.
 const data = new DataServices();
+// Fetch the trending memes from the data service and store them in a variable.
 const trendingMemes = await data.getTrendingMemes();
+// Get the container element where the trending memes will be displayed.
 const trendingContainer = document.getElementById('browse-results').querySelector('ul');
 
+// Display the trending memes on the home page using the renderListWithTemplate method of the DisplayServices module.
 renderListWithTemplate(trendingMemes, resultsTemplate, trendingContainer);
